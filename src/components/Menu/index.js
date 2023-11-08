bf572f778f38068476ad3771dd73ebb810591bad
Valtece <filhovaltece@gmail.com>
2023-11-08 00:17:08 -0300

    Atualização BlogNerd

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,25 @@
 import "./style.css";
 import logo from "./logo.png"
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../contexts/auth";
-import { useContext } from "react";
 
 export default function Menu(){
-  const { signed } = useContext(AuthContext);
-
-  console.log(signed);
+  
+  function menuShow() {
+    let menuMobile = document.querySelector(".mobile-menu");
+    if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+    } else {
+      menuMobile.classList.add('open')
+    }
+  }
 
   return (
     <div className="container">
       <div>
         <header>
           <nav>
-            <div>
-              <a href='/blognerd'>
+            <div className="box-logo">
+              <a href='/blognerd' className="link-img">
                 <img className="img-menu" src={logo}/>
               </a>
             </div>
@@ -27,12 +31,26 @@ export default function Menu(){
               <li><a href='#'>Jogos</a></li>
               <li><Link to="/login">Entrar</Link></li>
             </ul>
-            <div className="mobile">MENU</div>
+            <button onClick={menuShow} className="mobile">
+              <div className="traço"></div>
+              <div className="traço"></div>
+              <div className="traço"></div>
+            </button>
           </nav>
         </header>
+
+        <div className="mobile-menu">
+          <ul className="menu-mobile">
+            <li><a href='/blognerd'>Início</a></li>
+            <li><a href='#'>Tec</a></li>
+            <li><a href='#'>Séries</a></li>
+            <li><a href='#'>Filmes</a></li>
+            <li><a href='#'>Jogos</a></li>
+            <li><Link to="/login">Entrar</Link></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
   
 }
-
