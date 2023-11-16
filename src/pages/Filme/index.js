1a289526f89483b24172a233105a24cebd55257c
Valtece <filhovaltece@gmail.com>
2023-11-15 21:18:37 -0300

    Atualização Blognerd

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -2,9 +2,8 @@ import './style.css';
 
 export default function Filme() {
   return (
-    <div className="container">
+    <div className="filmes">
       <h1>Bem vindo a filmes</h1>
     </div>
   );
 }
-
